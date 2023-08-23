802f55d72677954e9e4448418f065eef0e531bc3
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-23 15:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const body = document.getElementsByTagName("body")[0];
+const div = document.createElement("div");
+div.classList.add("new");
+body.appendChild(div);
